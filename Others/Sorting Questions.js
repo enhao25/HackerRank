8f6e92d136147046b9ae5a8e643473d3f134d9cf
enhao25 <larloller@gmail.com>
2020-06-28 10:21:34 +0800

    Sorting Question in JavaScript added

---
+++ Others/Sorting Questions.js
@@ -0,0 +1,23 @@
+//#1 - Sort 10 schools around your house by distance:
+Insertion Sort (Small input)
+
+//#2 - eBay sorts listings by the current Bid amount:
+Merge sort X or quick sort X Radix / Count Sort (Bids is always numbers in a certain range)
+
+//#3 - Sport scores on ESPN
+Merge sort X or Quick sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+Quick Sort X Merge Sort (We will not be sorting in memeory anw, so space complexity is not an issue, but as we are worried about worst case, we should use Merge sort)
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+Insertion Sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+Quick sort (In case space complexity cannot take 50 years of data), did not use radix / counting sort as they doesn't support Temperature records with decimal
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+Merge Sort / Quick Sort
+
+//#8 - You want to teach sorting for the first time
+Insertion sort / bubble sort / selection sort
